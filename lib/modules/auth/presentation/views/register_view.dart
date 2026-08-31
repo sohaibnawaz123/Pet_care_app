9a2a9ff1be25308c4609c65c79996236bc358be0
Sohaib Nawaz <sohaib.nawaz@mmcgbl.com>
2026-08-31 15:41:28 +0500

    warning remove

--- lib/modules/auth/presentation/views/register_view.dart
+++ lib/modules/auth/presentation/views/register_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pet_care_app/component/button/app_button.dart';
 import 'package:pet_care_app/component/image/app_network_image.dart';
 import 'package:pet_care_app/component/text/content.dart';
